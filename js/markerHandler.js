2a504e22dd263f7cd0e0f006d8e63c82fb22f1a9
whitehatjr <52084703+whitehatjr@users.noreply.github.com>
2021-02-21 01:22:08 +0530

    Add files via upload

--- js/markerHandler.js
+++ js/markerHandler.js
@@ -1,3 +1,51 @@
+<<<<<<< HEAD
+AFRAME.registerComponent("markerhandler", {
+  init: async function () {
+
+    this.el.addEventListener("markerFound", () => {
+      console.log("marker is found")
+      this.handleMarkerFound();
+    });
+
+    this.el.addEventListener("markerLost", () => {
+      console.log("marker is lost")
+      this.handleMarkerLost();
+    });
+  },
+  handleMarkerFound: function () {
+    // Changing button div visibility
+    var buttonDiv = document.getElementById("button-div");
+    buttonDiv.style.display = "flex";
+
+    var ratingButton = document.getElementById("rating-button");
+    var orderButtton = document.getElementById("order-button");
+
+    // Handling Click Events
+    ratingButton.addEventListener("click", function () {
+      swal({
+        icon: "warning",
+        title: "Rate Dish",
+        text: "Work In Progress"
+      });
+    });
+
+    orderButtton.addEventListener("click", () => {
+      swal({
+        icon: "https://i.imgur.com/4NZ6uLY.jpg",
+        title: "Thanks For Order!",
+        text: "Your order will be served soon at your table!"
+      });
+    });
+  },
+
+  handleMarkerLost: function () {
+    // Changing button div visibility
+    var buttonDiv = document.getElementById("button-div");
+    buttonDiv.style.display = "none";
+  }
+});
+
+=======
 AFRAME.registerComponent("markerhandler", {
   init: async function () {
 
@@ -41,3 +89,4 @@ AFRAME.registerComponent("markerhandler", {
     buttonDiv.style.display = "none";
   }
 });
+>>>>>>> 3c443d81e625f81859f3e674f167935521faa203
