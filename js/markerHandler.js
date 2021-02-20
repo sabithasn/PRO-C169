eeb255e3ad6ae70a79027066c145b793316db3e5
PreetiSharma15 <pwhitehat6@gmail.com>
2021-02-21 01:29:50 +0530

    added create buttons

--- js/markerHandler.js
+++ js/markerHandler.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 AFRAME.registerComponent("markerhandler", {
   init: async function () {
 
@@ -44,49 +43,3 @@ AFRAME.registerComponent("markerhandler", {
     buttonDiv.style.display = "none";
   }
 });
-
-=======
-AFRAME.registerComponent("markerhandler", {
-  init: async function () {
-
-    this.el.addEventListener("markerFound", () => {
-      this.handleMarkerFound();
-    });
-
-    this.el.addEventListener("markerLost", () => {
-      this.handleMarkerLost();
-    });
-  },
-  handleMarkerFound: function () {
-    // Changing button div visibility
-    var buttonDiv = document.getElementById("button-div");
-    buttonDiv.style.display = "flex";
-
-    var ratingButton = document.getElementById("rating-button");
-    var orderButtton = document.getElementById("order-button");
-
-    // Handling Click Events
-    ratingButton.addEventListener("click", function () {
-      swal({
-        icon: "warning",
-        title: "Rate Dish",
-        text: "Work In Progress"
-      });
-    });
-
-    orderButtton.addEventListener("click", () => {
-      swal({
-        icon: "https://i.imgur.com/4NZ6uLY.jpg",
-        title: "Thanks For Order !",
-        text: "Your order will serve soon on your table!"
-      });
-    });
-  },
-
-  handleMarkerLost: function () {
-    // Changing button div visibility
-    var buttonDiv = document.getElementById("button-div");
-    buttonDiv.style.display = "none";
-  }
-});
->>>>>>> 3c443d81e625f81859f3e674f167935521faa203
